7c083628c0ab6a62756fec20773dece73eefafed
Darko <darkoristov14@gmail.com>
2021-05-22 22:54:49 +0100

    router

--- backend/portfolio/Contact/views.py
+++ backend/portfolio/Contact/views.py
@@ -1,20 +1,8 @@
-from rest_framework import serializers
-from rest_framework.response import Response
 from .models import Contact
 from .serializers import ContactSerializer
-from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import viewsets
 
-class ContactViewSet(APIView):
+class ContactViewSet(viewsets.ModelViewSet):
 
-    def get(self, request):
-        contact = Contact.objects.all()
-        serializer = ContactSerializer(contact, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ContactSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
--- backend/portfolio/portfolio/urls.py
+++ backend/portfolio/portfolio/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.db import router
 from django.urls import path, include
+from Contact import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('contact', views.ContactViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/', include('Contact.urls')),
+    path('', include(router.urls)),
     path('work/', include('Work.urls'))
 ]
